e351b4758696fe03d3342c47b57e2dfb8c01b2d1
pajkxf <1967935322@qq.com>
2025-03-12 10:52:17 +0800

    w

--- src/main/java/com/play/Main.java
+++ src/main/java/com/play/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("a");
         System.out.println("b");
-        System.out.println("d");
+        System.out.println("c");
         System.out.println("d");
         System.out.println("d");
         System.out.println("d");
