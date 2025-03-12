76a6387893c7927734e55c15356c410a747bf65c
pajkxf <1967935322@qq.com>
2025-03-12 10:16:20 +0800

    a

--- src/main/java/com/play/Main.java
+++ src/main/java/com/play/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("a");
         System.out.println("b");
-        System.out.println("d");
+        System.out.println("c");
     }
 }
