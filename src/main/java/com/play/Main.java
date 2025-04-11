d4e47294fe490c96b4af3efeb2bdfc7e9048f60e
git-wyy <2298631283@qq.com>
2025-04-11 12:38:58 +0800

    王永雁修改-第一次提交

--- src/main/java/com/play/Main.java
+++ src/main/java/com/play/Main.java
@@ -2,15 +2,6 @@ package com.play;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("a");
-        System.out.println("b");
-        System.out.println("c");
-        System.out.println("d");
-        System.out.println("d");
-        System.out.println("d");
-        System.out.println("d");
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
+        System.out.println("你好，世界！！！！");
     }
 }
