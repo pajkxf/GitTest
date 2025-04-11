4cf7012cae939f04e61fade91ec7cf469f5f4dec
git-wyy <2298631283@qq.com>
2025-04-11 12:52:42 +0800

    王永雁修改-第4次提交

--- src/main/java/com/play/Main.java
+++ src/main/java/com/play/Main.java
@@ -3,7 +3,7 @@ package com.play;
 public class Main {
     public static void main(String[] args) {
         System.out.println("你好，世界！！！！");
-        System.out.println("你好，世界！！！");
+        System.out.println("你好，世界！！");
         System.out.println("你好，世界！！！！");
         System.out.println("你好，世界！！！！");
     }
