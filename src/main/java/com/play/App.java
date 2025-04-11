57ccfe295487914e5a67c9df87e73b8a294bb571
pajkxf <1967935322@qq.com>
2025-04-11 23:34:14 +0800

    测试

--- src/main/java/com/play/App.java
+++ src/main/java/com/play/App.java
@@ -1,10 +1,19 @@
 package com.play;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author V
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        List<Integer> list1 = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+        list1.stream().filter(x -> x % 2 == 0).forEach(System.out::println);
     }
 }
